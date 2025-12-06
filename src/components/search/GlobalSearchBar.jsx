48702cd1c228e63db44b17f6a99c0604e9445eac
jesse <jesse@teleperson.com>
2025-12-06 15:30:12 +0000

    File changes

--- src/components/search/GlobalSearchBar.jsx
+++ src/components/search/GlobalSearchBar.jsx
@@ -42,7 +42,7 @@ export default function GlobalSearchBar() {
   };
 
   return (
-    <div className="relative flex-1 max-w-md">
+    <div className="relative flex-1 max-w-2xl">
       <div className="relative">
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400" />
         <input
